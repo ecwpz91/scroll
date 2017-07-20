c0ae2e71c177366d8e7d5f2cfcde27e9130ab09f
ecwpz91 <ecwpz91@users.noreply.github.com>
2017-07-20 00:54:27 -0400

    Removed bin/

--- n/networking/bin/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The --harmony parameter tells Node to use the latest ECMAScript Harmony features available.
-# ECMA Harmony is the code name for the next version of ECMAScript, the standard behind the JavaScript
-# language.
-node --harmony $1
