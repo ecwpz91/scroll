22ab176979489864be26cf98166689792768179f
ecwpz91 <ecwpz91@users.noreply.github.com>
2017-07-23 23:43:00 -0400

    Initial revision

---
+++ c/pun.c
@@ -0,0 +1,12 @@
+/*
+ * Name: Michael Surbey
+ * Purpose: Prints a bad pun.
+ * Author: K. N. Ling
+ * Date written: 5/21/95
+ */
+#include <stdio.h>
+
+int main() {
+	printf("To C, or not to C: this is the question.\n");
+	return 0;
+}
